a7aa5a6361e47c088b05701510594894e39deb55
Deyvis17GY <66072149+Deyvis17GY@users.noreply.github.com>
2020-11-01 18:43:46 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,16 +1,47 @@
-$('#ingresar').click(function (e) {
-    var usuario=$('#usuario').val();
-    var contraseña=$('#password').val();
+$(document).ready(function(){
     
-    if(usuario=="admin" && contraseña =="1234"){
-        console.log("ir");
-        window.location.href = "filtroProducto.html";
-       
-    }else{
-        alert("usuario Incorrecto")
-    }
+    $('#ingresar').click(function (e) {
+        var usuario=$('#usuario').val();
+        var contraseña=$('#password').val();
         
-    e.preventDefault();
+        if(usuario=="admin" && contraseña =="1234"){
+            console.log("ir");
+            window.location.href = "filtroProducto.html";
+           
+        }else{
+            alert("usuario Incorrecto")
+        }
+            
+        e.preventDefault();
+        
+    });
+   
+   
+        
     
+    
+   
 });
 
+
+
+    // NAVBAR ANTES DE HACER SCROLL
+window.addEventListener("scroll",function() {
+
+    var header = document.querySelector("nav");
+     
+    var a=document.getElementById("titulo");
+    var itemsA=document.getElementById("carrito");
+    var itemsB=document.getElementById("cuenta");
+    //var itemsC=document.getElementById("itemsC");
+    var flecha =document.getElementById("flecha");
+    header.classList.toggle("sticky",window.scrollY > 0);
+    a.classList.toggle("text-light",window.scrollY>0);
+    itemsA.classList.toggle("text-light",window.scrollY>0);
+    itemsB.classList.toggle("text-light",window.scrollY>0);
+    flecha.classList.toggle("text-light",window.scrollY>0);
+    
+  
+  })
+  
+  
