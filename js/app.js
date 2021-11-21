6df570271102e21078e152bdee914a295e0b987b
Deyvis17GY <deyvisjaime17@gmail.com>
2021-11-21 13:05:56 -0500

    ♻️ ✅ added readme and format document

--- js/app.js
+++ js/app.js
@@ -1,47 +1,31 @@
-$(document).ready(function(){
-    
-    $('#ingresar').click(function (e) {
-        var usuario=$('#usuario').val();
-        var contraseña=$('#password').val();
-        
-        if(usuario=="admin" && contraseña =="1234"){
-            console.log("ir");
-            window.location.href = "filtroProducto.html";
-           
-        }else{
-            alert("usuario Incorrecto")
-        }
-            
-        e.preventDefault();
-        
-    });
-   
-   
-        
-    
-    
-   
+$(document).ready(function () {
+  $('#ingresar').click(function (e) {
+    var usuario = $('#usuario').val();
+    var contraseña = $('#password').val();
+
+    if (usuario == 'admin' && contraseña == '1234') {
+      console.log('ir');
+      window.location.href = 'filtroProducto.html';
+    } else {
+      alert('usuario Incorrecto');
+    }
+
+    e.preventDefault();
+  });
 });
 
+// NAVBAR ANTES DE HACER SCROLL
+window.addEventListener('scroll', function () {
+  var header = document.querySelector('nav');
 
-
-    // NAVBAR ANTES DE HACER SCROLL
-window.addEventListener("scroll",function() {
-
-    var header = document.querySelector("nav");
-     
-    var a=document.getElementById("titulo");
-    var itemsA=document.getElementById("carrito");
-    var itemsB=document.getElementById("cuenta");
-    //var itemsC=document.getElementById("itemsC");
-    var flecha =document.getElementById("flecha");
-    header.classList.toggle("sticky",window.scrollY > 0);
-    a.classList.toggle("text-light",window.scrollY>0);
-    itemsA.classList.toggle("text-light",window.scrollY>0);
-    itemsB.classList.toggle("text-light",window.scrollY>0);
-    flecha.classList.toggle("text-light",window.scrollY>0);
-    
-  
-  })
-  
-  
+  var a = document.getElementById('titulo');
+  var itemsA = document.getElementById('carrito');
+  var itemsB = document.getElementById('cuenta');
+  //var itemsC=document.getElementById("itemsC");
+  var flecha = document.getElementById('flecha');
+  header.classList.toggle('sticky', window.scrollY > 0);
+  a.classList.toggle('text-light', window.scrollY > 0);
+  itemsA.classList.toggle('text-light', window.scrollY > 0);
+  itemsB.classList.toggle('text-light', window.scrollY > 0);
+  flecha.classList.toggle('text-light', window.scrollY > 0);
+});
